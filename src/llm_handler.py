2d56dda9d90509dd639c0b46fc0c09eb138e9d5e
jwgcurrie <joelcurrie3@gmail.com>
2025-07-11 14:08:47 +0200

    Increased temperature to max

--- src/llm_handler.py
+++ src/llm_handler.py
@@ -61,7 +61,7 @@ class LLMHandler:
                 **inputs,
                 max_new_tokens=256,
                 do_sample=True,
-                temperature=0.7,
+                temperature=0.99,
                 top_p=0.95,
                 pad_token_id=self.tokenizer.pad_token_id,
                 eos_token_id=self.tokenizer.eos_token_id,
